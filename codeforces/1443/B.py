6bedff715c36cd689d6370ffe6737e8bb94efd8f
Kunal Makwana <kunalmakwana18@gnu.ac.in>
2021-02-11 02:19:00 +0530

    Add solution for problem `B - Saving the City` Link: https://codeforces.com/contest/1443/problem/B Tags: dp, greedy, math, sortings, *1300 Ref: https://codeforces.com/contest/1443/submission/107088912

---
+++ codeforces/1443/B.py
@@ -0,0 +1,94 @@
+#===========Template===============
+from io import BytesIO, IOBase
+import sys,os
+inpl=lambda:list(map(int,input().split()))
+inpm=lambda:map(int,input().split())
+inpi=lambda:int(input())
+inp=lambda:input()
+rev,ra,l=reversed,range,len
+P=print
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+def input(): return sys.stdin.readline().rstrip("\r\n")
+
+#=========I/p O/p =================
+import sys 
+# sys.stdin = open('input.txt', 'r')   
+# sys.stdout = open('output.txt', 'w')
+
+#==============To chaliye shuru krte he ====================#
+
+test=inpi()
+while test:
+    a,b=inpm()
+    st=list(input())
+    n=len(st)
+    i=0
+    while i<n and st[i]=="0":
+        i+=1
+    j=n-1
+    while j>=0 and st[j]=="0":
+        j-=1
+    tr=False
+    ans=float('inf')
+    count=0
+    while i<n:
+        if tr:
+            if st[i]=="1" and st[i-1]=="0":
+                ans=min(ans+a,(count*b)+ans)
+                count=0
+            elif st[i]=="0":
+                count+=1
+        else:
+            ans=a
+            tr=True
+        i+=1
+    if ans==float('inf'):
+        P(0)
+    else:P(ans)
+    test-=1
+        
