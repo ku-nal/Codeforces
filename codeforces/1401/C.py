d5b901a0208923cb30a9a5d554f48e6424076c5d
Kunal Makwana <kunalmakwana18@gnu.ac.in>
2021-02-14 01:57:00 +0530

    Add solution for problem `C - Mere Array` Link: https://codeforces.com/contest/1401/problem/C Tags: constructive algorithms, math, number theory, sortings, *1300 Ref: https://codeforces.com/contest/1401/submission/107304421

---
+++ codeforces/1401/C.py
@@ -0,0 +1,83 @@
+#===========Template===============
+from io import BytesIO, IOBase
+import sys,os
+inpl=lambda:list(map(int,input().split()))
+inpm=lambda:map(int,input().split())
+inpi=lambda:int(input())
+inp=lambda:input()
+rev,ra,l=reversed,range,len
+P=print
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+def input(): return sys.stdin.readline().rstrip("\r\n")
+
+#=========I/p O/p =================
+import sys,math
+
+
+#==============To chaliye shuru krte he ====================#
+tc=inpi()
+while tc:
+    n=inpi()
+    li=inpl()
+    if n==1:
+        P("Yes")
+    else:
+        li1=li.copy()
+        li1.sort()
+        tr=True
+        mi=li1[0]
+        for i in ra(n):
+            if li1[i]!=li[i]:
+                if math.gcd(li[i],mi)!=mi:
+                    tr=False
+                    break
+        if tr:
+            P("Yes")
+        else:
+            P("No")
+    tc-=1
