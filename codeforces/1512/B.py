372469a432bc642c6d02d366af95e69119110638
Kunal Makwana <kunalmakwana18@gnu.ac.in>
2021-04-10 20:32:00 +0530

    Add solution for problem `B - Almost Rectangle` Link: https://codeforces.com/contest/1512/problem/B Tags: implementation, *800 Ref: https://codeforces.com/contest/1512/submission/112494293

---
+++ codeforces/1512/B.py
@@ -0,0 +1,100 @@
+#===========Template===============
+from io import BytesIO, IOBase
+import sys,os
+inpl=lambda:list(map(int,input().split()))
+inpm=lambda:map(int,input().split())
+inpi=lambda:int(input())
+inp=lambda:input()
+rev,ra,l=reversed,range,len
+
+P=print
+BUFSIZE = 8192
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+def input(): return sys.stdin.readline().rstrip("\r\n")
+
+#=========I/p O/p ========================================#
+from bisect import bisect_left as bl 
+from bisect import bisect_right as br
+import sys,operator,math,operator
+from collections import Counter 
+import random
+#==============To chaliye shuru krte he ====================#
+tc=inpi()
+while tc:
+    tc-=1
+    n=inpi()
+    li=[]
+    for i in ra(n):
+        li.append(list(input()))
+    pos=[]
+    for i in ra(n):
+        for j in ra(n):
+            if li[i][j]=="*":
+                pos.append([i,j])
+    
+    a,b=pos 
+  
+    if a[0]==b[0]:
+        for i in rev(ra(0,n)):
+            
+            if i!=a[0]:
+                pad=[[i,a[1]],[i,b[1]]]
+                break
+    elif a[1]==b[1]:
+        for i in rev(ra(0,n)):
+            if i!=a[1]:
+            
+                pad=[[a[0],i],[b[0],i]]
+                break
+    else:
+        pad=[[a[0],b[1]],[b[0],a[1]]]
+    for i in ra(n):
+        for j in ra(n):
+            if [i,j] in pad:
+                li[i][j]="*"
+    for i in ra(n):
+        P(''.join(li[i]))
+          
